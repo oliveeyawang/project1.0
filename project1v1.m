17ced1d0c50bd65f57120997eaaa9cdfa9b9b504
kcidiaz <kiara_ibanez-diaz@brown.edu>
2025-03-11 16:01:32 -0400

    Writing some code

--- project1v1.m
+++ project1v1.m
@@ -1,4 +1,7 @@
-testing 
-disp('hello world')
 
-it's alive!!!
+scrsz = get(groot, 'ScreenSize');
+
+MAIN = figure ('Position', scrsz, 'Color',[0,1,1]);
+% left to right, bottom to top, width, height
+% color: RGB
+

84884057adbb453d7648abd673d2bc3ab19a1919
kcidiaz <kiara_ibanez-diaz@brown.edu>
2025-03-12 19:51:50 -0400

    What We have so far (did not add new pages yet, it doesn't work)

--- project1v1.m
+++ project1v1.m
@@ -1,29 +1,59 @@
 
-
-uiwait(msgbox('Open our awesome game?'));
-
-
-
-
-%figure for game
 scrsz = get(groot, 'ScreenSize');
-MAIN = figure ('Position', scrsz, 'Color',[0,1,1]);
+
+banana_milk = [1,0.831,0.431];
+
+muted_per = [0.698, 0.318, 0.169];
+
+tan_orange = [0.925, 0.624, 0.169];
+
+green = [0.349, 0.8, 0.02];
+
+start_screen = figure('Position', scrsz, 'Color', [1, 1, 1], 'Name', 'Start Screen', 'NumberTitle', 'off');
+
+axes('Position', [0.3, 0.5, 0.4, 0.3]); 
+img = imread('puppy_image.jpg');
+imshow(img);
+
+
+uicontrol('Style', 'text', ...
+    'String', 'WELCOME TO', ...
+    'Units', 'normalized', ...
+    'Position', [0.2, 0.7, 0.3, 0.1], ... 
+    'FontSize', 24, ...
+    'FontWeight', 'bold', ...
+    'BackgroundColor', [1, 1, 1]);
+
+
+
+uicontrol('Style', 'pushbutton', ...
+    'String', 'Play Our Awesome Game!', ...
+    'Units', 'normalized', ...
+    'Position', [0.38, 0.2, 0.26, 0.08], ...
+    'FontSize', 20, ...
+    'FontWeight', 'bold', ...
+    'BackgroundColor', green, ...
+    'Callback', @(src, event) uiresume(start_screen),...
+    'ForegroundColor',[1,1,1]);
+
+uiwait(start_screen);
+close(start_screen);
 % left to right, bottom to top, width, height
 % color: RGB
 
 %%
 %figure for game modes
 
-gmback_color = [0.839,0.733,0.941];
-game_mode = figure ('Units', 'normalized','Position', [.16, .16, .7, .7], 'Color', gmback_color);
+
+game_mode = figure ('Units', 'normalized','Position', [.16, .16, .7, .7], 'Color', banana_milk);
 
 %Text code
 TextType = uicontrol ('Style','text', 'Units','normalized', 'Position',[.265,.5,.5,.3],...
-    'String', 'WHICH GAME MODE WOULD YOU LIKE TO PLAY?', 'Backgroundcolor',gmback_color,...
+    'String', 'WHICH GAME MODE WOULD YOU LIKE TO PLAY?', 'Backgroundcolor',banana_milk,...
     'FontSize',28, 'FontWeight','bold', 'ForegroundColor',[1,1,1], 'FontUnits','normalized');
 
 %testing puppy image (you could draw something for it)
-image_1 = imread('puppy_image.jpg');
+puppy_image = imread('puppy_image.jpg');
 % 'CData',image_1
 
 
@@ -36,30 +66,52 @@ timecha = uicontrol('Style','pushbutton', 'Units', 'normalized','Position', [.62
      'UserData','timecha');
 
 StartGame = uicontrol('Style','pushbutton', 'Units','normalized',...
-        'Position', [.265, .15, .14, .12], 'Visible','off');
+        'Position', [.41, .3, .18, .1], 'Visible','off', 'FontSize', 20, 'FontWeight', 'bold',...
+        'ForegroundColor',[1,1,1], 'BackgroundColor',green);
+
+
 
 set(five_normal, 'Callback', {@user_mode, five_normal, timecha, StartGame});
 set(timecha, 'Callback', {@user_mode, five_normal, timecha, StartGame});
 
 % Button function
-function user_mode (button, event, five_normal, timecha, StartGame)
+function user_mode (button, ~, five_normal, timecha, StartGame)
 
 % Allows for switch between buttons
 if strcmpi(button.UserData, 'five_normal')
     five_normal.Enable = 'on';
-    timecha.Enable = 'off';
-    StartGame.String = 'START';
+    timecha.Enable = 'on';
+    StartGame.String = 'Ready!';
     StartGame.Visible = 'on';
 
+   set(StartGame, 'Callback', @(src, event) create_mode_screen('Five Normal', scrsz));
 
 elseif strcmpi(button.UserData, 'timecha')
-    five_normal.Enable = 'off';
+    five_normal.Enable = 'on';
     timecha.Enable = 'on';
     StartGame.String = 'START';
     StartGame.Visible = 'on';
+
+    set(StartGame, 'Callback', @(src, event) create_mode_screen('Time Challenge', scrsz));
     
 else 
     fprintf('shouldn''t happen, error')
 end
 end
 
+function create_mode_screen(mode, scrsz)
+    % Close previous figure
+    close all;
+
+    % Create the figure for the selected game mode
+    mode_screen = figure('Position', scrsz);
+
+    % Set the title based on the selected mode
+    uicontrol('Style', 'text', 'String', [mode ' Mode'], 'FontSize', 30, 'Position', [0.4, 0.5, 0.2, 0.2]);
+
+    % Customize the figure for the selected game mode here
+    % Example: add game logic, buttons, etc.
+    % You can add the game interface here (e.g., text, buttons, etc.)
+    % Just as a placeholder:
+    uicontrol('Style', 'pushbutton', 'String', ['Start ' mode], 'FontSize', 20, 'Position', [0.4, 0.4, 0.2, 0.1]);
+end
